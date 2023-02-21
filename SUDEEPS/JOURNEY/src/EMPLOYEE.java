c6e3662caee3188bd6a4cbdad68110863cf59219
sudeep7978 <111543327+sudeep7978@users.noreply.github.com>
2023-02-21 15:50:03 +0530

    Add files via upload

---
+++ SUDEEPS/JOURNEY/src/EMPLOYEE.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class EMPLOYEE {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		
+		
+			
+		Scanner sc = new Scanner(System.in);
+		
+		
+	EMPLOYEE s1 = new EMPLOYEE();
+
+	
+	
+		System.out.println("please enter the firstname");
+		System.out.println("please enter the lastname");
+		String str = sc.nextLine();
+		String str1 = sc.nextLine();
+		
+		
+		System.out.println("enter your department   (techical/admin/human resource/legal)");
+		String str2 = sc.nextLine();
+		System.out.println(str + str1 + "@" + str2 + "gmail.com");
+
+
+		 
+
+		
+	}
+
+}
